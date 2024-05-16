afb311e06ee790c0c0f39fe53c426da8684deb61
yosefbenzo <yosef.mohamed2312@gmail.com>
2024-05-17 00:22:21 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,171 @@
+// Q1 : 
+
+// let num = prompt("please enter a number : ")
+// let result = parseInt(num)
+// console.log("the number is : " +result);
+
+////////////////////////////////////////////////////////////////
+
+// Q2 : 
+
+// var num = prompt("Please enter a number:");
+// if (num % 3 === 0 && num % 4 === 0) {
+
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+////////////////////////////////////////////////////////////////
+
+// Q3:
+// var firstnum = parseInt(prompt("Please enter the first integer:"));
+
+// var secondnum = parseInt(prompt("Please enter the second integer:"));
+
+// if (firstnum > secondnum) {
+//     console.log("The max is:", firstnum);
+// } else {
+//     console.log("The max is:", secondnum);
+// }
+
+////////////////////////////////////////////////////////////////
+
+// Q4:
+// var num = parseInt(prompt("Please enter the number:"));
+
+// if (num < 0) {
+//     console.log("negative");
+// } else {
+//     console.log("positive");
+// }
+
+////////////////////////////////////////////////////////////////
+
+// Q5 : 
+// var firstNum = parseInt(prompt("Please enter the first number:"));
+// var secondNum = parseInt(prompt("Please enter the second number:"));
+// var thirdNum = parseInt(prompt("Please enter the third number:"));
+
+// var maxNum = firstNum;
+// var minNum = firstNum;
+
+// if (secondNum > maxNum) {
+//     maxNum = secondNum;
+// }
+// if (thirdNum > maxNum) {
+//     maxNum = thirdNum;
+// }
+// if (secondNum < minNum) {
+//     minNum = secondNum;
+// }
+// if (thirdNum < minNum) {
+//     minNum = thirdNum;
+// }
+// console.log("Max number is :", maxNum);
+// console.log("Min number is :", minNum);
+ 
+
+////////////////////////////////////////////////////////////////
+
+// Q6 : 
+// var Num = parseInt(prompt("Please enter a number:"));
+
+// if (Num % 2 === 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+////////////////////////////////////////////////////////////////
+
+// Q8 :
+
+// var char = prompt(" Please enter a character:");
+
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//     console.log("vowel");
+// } else {
+//     console.log("consonant");
+// }
+
+////////////////////////////////////////////////////////////////
+
+// Q9 : 
+
+// var num = prompt("Please enter a number :");
+
+//     for (var i = 1; i <= num; i++) {
+//         console.log(i);
+//     }
+
+////////////////////////////////////////////////////////////////
+
+// Q10 :
+
+// var num = prompt("Please enter a number:");
+
+//     for (var i = 1; i <= 12; i++) {
+//         console.log((num * i));
+//     }
+
+
+////////////////////////////////////////////////////////////////
+
+// Q11 : 
+
+// var num = prompt("Please enter a number:");
+
+//     for (var i = 2; i <= num; i += 2) {
+//         console.log(i);
+//     }
+
+////////////////////////////////////////////////////////////////
+
+// Q12 : 
+// var base = prompt("Please enter the base number:");
+
+// var power = prompt("Please enter the power:");
+
+// var result = 1;
+
+//     for (var i = 0; i < power; i++) {
+//         result *= base;
+//     }
+
+//     console.log(result);
+
+// Q7 : 
+// var marks = [];
+// var totalMarks = 0;
+
+// for (var i = 0; i < 5; i++) {
+//     marks[i] = parseFloat(prompt(" Please enter marks for subject " + (i + 1) + ":"));
+//     totalMarks += marks[i];
+// }
+// var totalSubjects = 5;
+// var averageMarks = totalMarks / totalSubjects;
+// var percentage = (totalMarks / (totalSubjects * 100)) * 100;
+// var totalMaxMarks = totalSubjects * 100; 
+// var percentage = (totalMarks / totalMaxMarks) * 100;
+
+// console.log("Total marks:", totalMarks,);
+// console.log("Average marks:", averageMarks);
+// console.log("Percentage:", percentage+ "%");
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
